a7cd5778aaf14db871cc0958785271c8143b4815
PiyushxBajaj <piyushpiyush41@gmail.com>
2019-06-19 22:51:23 +0530

    Initial Commit

---
+++ imdbApiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imdbApiApp
+//
+//  Created by Piyush Bajaj on 6/19/19.
+//  Copyright © 2019 Piyush Bajaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
